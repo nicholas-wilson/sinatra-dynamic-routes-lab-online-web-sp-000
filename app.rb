cacd384351a2a634cb955fc578ab5cd6c40f288b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 03:55:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    raise params
+    raise params.inspect
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @number.times do
