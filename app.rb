4fc7404c3bd30f1a9950f859dcfbaadea004798c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 03:38:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/' do
+    'Basic Page'
+  end
 
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+
+  end
 end
